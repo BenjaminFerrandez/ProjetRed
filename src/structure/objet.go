7b883740c66ad0c4035d2711ab406010f33424ca
Alexandre <alexandre.queudeville@ynov.com>
2023-09-22 09:23:14 +0200

    y

--- src/structure/objet.go
+++ src/structure/objet.go
@@ -9,7 +9,7 @@ type Object struct {
 }
 
 func Obj() {
-    Health_potion := []string{"Health potion:", "Donne X15P", "Coute 25 $"}
+    Health_potion := []string{"Health potion:", "Donne 15HP", "Coute 25 $"}
     Poison_potion := []string{"Poison potion:", "Enlève 15HP à l'adversaire", "Coute 25 $"}
     Upgrade_potion := []string{"Upgrade potion:", "Améliore tes attaques de 20% sur ton prochain tour", "Coute 50 $"}
     Shield := []string{"Shield:", "Réduit les dégats de 15% de la prochaine attaque subit", "Coute 40 $"}
