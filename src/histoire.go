059574efecff529a50edf6ceae83ec376cc635db
Alexandre <alexandre.queudeville@ynov.com>
2023-10-02 10:31:54 +0200

    y

--- src/histoire.go
+++ src/histoire.go
@@ -1,26 +1,48 @@
 package main
 
 import (
-	"fmt"
+	"image/color"
 	"time"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
-func history() {
-	time.Sleep(time.Second * 1)
-	fmt.Println("Welcome in our world !")
-	time.Sleep(time.Second * 1)
-	fmt.Println("A world where people have some magical power.")
-	time.Sleep(time.Second * 2)
-	fmt.Println("You will be one of them to complete a mission")
-	time.Sleep(time.Second * 2)
-	fmt.Println("The mission is to kill a monster but we know one specific thing about him.")
-	time.Sleep(time.Second * 3)
-	fmt.Println("The thing that you need to know is that when you kill him, he revive with a little bit more of life.")
-	time.Sleep(time.Second * 4)
-	fmt.Println("The only way to kill him definitely is to kill him 4 time in a row")
-	time.Sleep(time.Second * 3)
-	fmt.Println("To succeed, you can buy some item that will help you so do not hesitate (if you have enough money of course)")
-	time.Sleep(time.Second * 3)
-	fmt.Println("Good luck and enjoy !")
-	time.Sleep(time.Second * 2)
+var (
+    historyLines = []string{
+        "Welcome in our world !",
+        "A world where people have some magical power.",
+        "You will be one of them to complete a mission",
+        "The mission is to kill a monster but we know one specific thing about him.",
+        "The thing that you need to know is that when you kill him, he revives with a little bit more of life.",
+        "The only way to kill him definitely is to kill him 4 times in a row",
+        "To succeed, you can buy some items that will help you, so do not hesitate (if you have enough money, of course)",
+        "Good luck and enjoy !",
+    }
+    currentLineIndex int
+    displayedText    string
+    lastUpdateTime   time.Time
+)
+
+func history(screen *ebiten.Image) error {
+
+    screen.Fill(color.Black)
+
+
+    if currentLineIndex < len(historyLines) {
+        if time.Since(lastUpdateTime) >= time.Second*2 {
+            displayedText += historyLines[currentLineIndex] + "\n"
+            currentLineIndex++
+            lastUpdateTime = time.Now()
+        }
+    }
+
+ 
+    textX := 100 
+    textY := 100 
+
+
+    ebitenutil.DebugPrintAt(screen, displayedText, textX, textY)
+
+    return nil
 }
--- src/main.go
+++ src/main.go
@@ -1,18 +1,24 @@
 package main
 
-import (
-    "fmt"
+import ("fmt"
     "game/structure" //permet l'accès aux fichiers du dossier structure
-    "time"
-)
 
+    "github.com/hajimehoshi/ebiten"
+)
+const (
+    screenWidth  = 800
+    screenHeight = 600
+)
+var screen *ebiten.Image
 //première fonction lancée
 func main() {
     for {
+        ebiten.Run(history, screenWidth, screenHeight, 1, "Death Back")
+      
         fmt.Println("D E A T H   B A C K") //titre
-        time.Sleep(time.Second * 2)
-        fmt.Println("\nHello !")
-        history()
+ 
+ 
+
         fmt.Println("\nClick 's' to start or 'q' to quit") //premier choix du jeu, commencer ou quitter
         var choice string //choice va servir pour les choix
         fmt.Scanln(&choice)
